9867264c2ac2f853e8c7ab5f743a1d413b584e6c
root <root@laszlo.h1.pl>
2022-06-24 16:03:24 +0200

    first commit

---
+++ data.sql
@@ -0,0 +1,21 @@
+insert into educations (education, seq)
+values
+	('podstawowe', 1),
+	('gimnazjalne', 2),
+	('zasadnicze zawodowe', 3),
+	('zasadnicze branżowe', 4),
+	('średnie branżowe', 5),
+	('średnie', 6),
+	('wyższe', 7)
+;
+
+insert into jobs (job)
+values
+	('nauczyciel'),
+	('urzędnik'),
+	('policjant'),
+	('strażak'),
+	('lekarz'),
+	('prawnik'),
+	('inny')
+;
